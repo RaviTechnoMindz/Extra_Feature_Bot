e3aa1aa6b5d9c60712f9aad00d0d5bb765411e17
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2022-08-09 18:13:26 +0530

    BUG were killed

--- plugins/commands.py
+++ plugins/commands.py
@@ -70,7 +70,6 @@ async def start(client, message):
         m=await message.reply_sticker("CAACAgIAAxkBAALdtWKbQQeiW14ql5zd86zLUzJO_HCVAAKHFQACPQ9QSd6bZv3Wx6O_HgQ") 
         await asyncio.sleep(2)
         await m.delete()
-        await message.reply_chat_action("typing")
         await message.reply_video(
             video=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
