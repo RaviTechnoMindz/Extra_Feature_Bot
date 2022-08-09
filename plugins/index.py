e5b5bbd69b37596c998f9d0bc3f02a415c00328d
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2022-08-09 19:06:40 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -144,10 +144,46 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
         try:
             current = temp.CURRENT
             temp.CANCEL = False
-            async for message in bot.iter_messages(chat, lst_msg_id, temp.CURRENT):
+            while current < total:
                 if temp.CANCEL:
                     await msg.edit(f"Successfully Cancelled!!\n\nSaved <code>{total_files}</code> files to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>")
                     break
+                try:
+                    message = await bot.get_messages(chat_id=chat, message_ids=current, replies=0)
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+                    message = await bot.get_messages(
+                        chat,
+                        current,
+                        replies=0
+                    )
+                except Exception as e:
+                    logger.exception(e)
+                try:
+                    for file_type in ("document", "video", "audio"):
+                        media = getattr(message, file_type, None)
+                        if media is not None:
+                            break
+                        else:
+                            continue
+                    media.file_type = file_type
+                    media.caption = message.caption
+                    aynav, vnay = await save_file(media)
+                    if aynav:
+                        total_files += 1
+                    elif vnay == 0:
+                        duplicate += 1
+                    elif vnay == 2:
+                        errors += 1
+                except Exception as e:
+                    if "NoneType" in str(e):
+                        if message.empty:
+                            deleted += 1
+                        elif not media:
+                            no_media += 1
+                        logger.warning("Skipping deleted / Non-Media messages (if this continues for long, use /setskip to set a skip number)")     
+                    else:
+                        logger.exception(e)
                 current += 1
                 if current % 20 == 0:
                     can = [[InlineKeyboardButton('Cancel', callback_data='index_cancel')]]
